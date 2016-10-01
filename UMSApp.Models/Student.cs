2a237995ec35be169955aea2a87df0e04bd95405
mustafizur8888 <mustafizur8888@gmail.com>
2016-10-01 23:59:40 +0600

    Initial commit

---
+++ UMSApp.Models/Student.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UMSApp.Models
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Index("Ix_StudentName")]
+        public string Name { get; set; }
+
+
+    }
+
+    public class Department
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Index("Ix_DepartmentName")]
+        public string Name { get; set; }
+
+    }
+}
